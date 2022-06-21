69b799ceb55a6f954b166223bf6eea90fa514ce1
wi21b095 <wi21b095@technikum-wien.at>
2022-06-21 17:09:50 +0200

    change RequestMapping to GetMappung

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -1,5 +1,6 @@
 package com.example.calculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
-    @RequestMapping("/sum")
+    @GetMapping("/sum");
     public int sum(@RequestParam int a, @RequestParam int b){
         return a + b;
     }

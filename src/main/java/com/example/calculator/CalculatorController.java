4b5b45cc100b76b6da378ac3c7f73c95e506142a
wi21b095 <wi21b095@technikum-wien.at>
2022-06-21 17:11:33 +0200

    removed typo

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
-    @GetMapping("/sum");
+    @GetMapping("/sum")
     public int sum(@RequestParam int a, @RequestParam int b){
         return a + b;
     }
